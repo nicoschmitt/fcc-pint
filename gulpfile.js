10e5985386c61ab2570bd96a835a382015250da3
Nicolas Schmitt <scalpmail@gmail.com>
2016-03-16 09:53:18 +0100

    move scss to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,29 @@ var del         = require('del');
 var concat      = require("gulp-concat");
 var uglify      = require("gulp-uglify");
 var rename      = require("gulp-rename");
+var htmlbuild   = require("gulp-htmlbuild");
+var sass        = require("gulp-sass");
+var cssnano     = require("gulp-cssnano");
+
+// Auto
+
+gulp.task('sass', function() {
+    gulp.src('client/css/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest(function(f) {
+            return f.base;
+        }))
+});
+
+gulp.task("watch", function() {
+   gulp.watch('client/css/*.scss', ['sass']); 
+});
+
+// Dev
 
 gulp.task("clean", function() {
    del("client/lib/*"); 
-});
-
-gulp.task("app-script", function() {
-    return gulp.src("client/app/scripts/*.js")
-               .pipe(concat("myapp.min.js"))
-               .pipe(uglify())
-               .pipe(gulp.dest("client/lib"));
 });
 
 gulp.task('bower-js', function() {
@@ -41,4 +54,25 @@ gulp.task('bower-min-css', function() {
                .pipe(gulp.dest("client/lib"));
 });
 
+// Production
+
+gulp.task("app-script", function() {
+    return gulp.src("client/app/scripts/*.js")
+               .pipe(concat("myapp.min.js"))
+               .pipe(uglify())
+               .pipe(gulp.dest("client/lib"));
+});
+
+gulp.task("replace-js", function() {
+    return gulp.src("client/index.html")
+               .pipe(htmlbuild({
+                   
+               }))
+               .pipe(gulp.dest("client/lib"));
+});
+
+
+// Main tasks
+
 gulp.task('default', ["clean", 'bower-js', 'bower-min-js', 'bower-css', 'bower-min-css']);
+gulp.task("production", ["app-script", "replace-js"]);
--- server.js
+++ server.js
@@ -6,22 +6,11 @@ var mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI);
 
 var express = require('express');
-var sassMiddleware = require('node-sass-middleware');
-
 var bodyParser = require('body-parser');
-
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.use(sassMiddleware({
-    src: path.join(__dirname, "scss"),
-    dest: path.join(__dirname, "client/css"),
-    prefix: "/css",
-    outputStyle: 'compressed',
-    debug: false
-}));
 
 require('./server/routes').register(app);
 
